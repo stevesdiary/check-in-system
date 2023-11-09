1523340aba3585e5af56a0c753c761637e18cb0e
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2023-11-09 23:54:01 +0100

    Cleanups

--- routes/Visitor.js
+++ routes/Visitor.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const moment = require("moment");
 const app = express();
 const { Device, Visitor, Sequelize} = require('../models');
-const Op = Sequelize.Op;
 const { authentication } = require('../middleware/authentication');
 const verifyRoles = require('../middleware/verifyRoles');
 const roles_list = require('../config/roles_list');
@@ -11,196 +10,6 @@ const roles_list = require('../config/roles_list');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Fetch current visitors In-house
-// router.get('/current-visitors/', authentication, async (req, res) => {
-//    try {
-//       const search = req.query.search;
-//       let name_purpose_search = []
-//       if (search) {
-//          name_purpose_search.push(
-//             {
-//                [Op.or]: [
-//                   { first_name: { [Op.like]: `%${search}%` } },
-//                   { last_name: { [Op.like]: `%${search}%` } },
-//                   { purpose: { [Op.like]: `%${search}%` } }
-//                ]
-//             },
-//          )
-//       }
-//       const count = await Visitor.count({
-//          where: {
-//             departure: { [Op.eq]: null },
-//          [Op.and]:  [
-//             ...name_purpose_search
-//          ],
-//          }
-//       });
-//       let per_page;
-//       if (count < 10) {
-//          per_page = count
-//       } if (count > 9) {
-//          per_page = 10
-//       }
-//       const page = parseInt(req.query.page) || 1;
-//       const from = (page - 1) * per_page;
-//       const to = page * per_page;
-//       // const end = Math.min(startIndex + itemsPerPage, totalItems);
-//       const total_pages = Math.ceil(count / per_page);
-//       // const currentPage = total_pages
-//       let visitor = await Visitor.findAll({
-//          where: {
-//             departure: { [Op.eq]: null },
-//          [Op.and]:  [
-//             ...name_purpose_search
-//          ],
-//          },
-//          include: [{
-//             model: Device
-//          }],
-//          page: { [Op.eq]: `${page}` },
-//          order: [
-//             [Sequelize.fn('date', Sequelize.col('date')), 'DESC'],
-//             [Sequelize.fn('time', Sequelize.col('arrival')), 'DESC'],
-            
-//          ],
-//       })
-//       if (page) {
-//          visitor = visitor.slice((10 * page) - 10, 10 * page)
-//       }
-//       console.log(visitor)
-//       if (visitor.length > 0){
-//          return res.status(200).send({
-//             statusCode: 200,
-//             page: page,
-//             per_page: per_page,
-//             total_records: count,
-//             total_pages: total_pages,
-//             showing_from: from + 1,
-//             to: to,
-//             visitor: visitor
-//          })
-//       }
-      
-//       if (count === 0){
-//          return res.status(200).send({
-//             message: 'No record found for this request',
-//          })
-//       }
-//    } catch (err) {
-//       console.log(err)
-//       return res.status(500).send({ message: err.message })
-//    }
-// });
-// // Fetch visitors' History
-// router.get('/allvisitors/', authentication, async (req, res) => {
-//    try {
-//       const start_date = (req.query.start_date) || "2022-10-01";
-//       const end_date = (req.query.end_date) || moment().format("YYYY-MM-DD");
-//       const search = req.query.search;
-//       let name_purpose_search = []
-//       if (search) {
-//          name_purpose_search.push({
-//             [Op.or]: [
-//                { first_name: { [Op.like]: `%${search}%` } },
-//                { last_name: { [Op.like]: `%${search}%` } },
-//                { purpose: { [Op.like]: `%${search}%` } },
-//                { host: { [Op.like]: `%${search}%` } },
-//             ],
-//          });
-//       }
-//       const count = await Visitor.count({
-//          where: {
-//             departure: { [Op.not]: null },
-//             [Op.and]: [
-//                ...name_purpose_search,
-//                Sequelize.literal(`date BETWEEN '${start_date}' AND '${end_date}'`),
-//             ],
-//          }
-//       });
-//       let per_page;
-//       if (count < 10){
-//          per_page = count
-//       }
-//       if (count > 9){
-//          per_page = 10
-//       }
-      
-//       const page = parseInt(req.query.page) || 1;
-//       const from = (page - 1) * per_page;
-//       const to = page * per_page
-//       const total_pages = Math.ceil(count / per_page);
-      
-//       let visitor = await Visitor.findAll({
-//          where: {
-//             departure: { [Op.not]: null },
-//             [Op.and]: [
-//                ...name_purpose_search,
-//                Sequelize.literal(`date BETWEEN '${start_date}' AND '${end_date}'`),
-//             ],
-//          },
-//          include: [{
-//             model: Device
-//          }],
-//          order: [
-//             // [Sequelize.fn('time', Sequelize.col('createdAt')), 'DESC'],
-//             [Sequelize.fn('time', Sequelize.col('departure')), 'DESC'],
-//             [Sequelize.fn('date', Sequelize.col('date')),'DESC']            
-//          ]
-//       })
-//       if (page) {
-//          visitor = visitor.slice((10 * page) - 10, 10 * page)
-//       }
-//       if (visitor.length > 0) {
-//          return res.status(200).send({
-//             statusCode: 200,
-//             page: page,
-//             per_page: per_page,
-//             total_records: count,
-//             total_pages: total_pages,
-//             showing_from: from + 1,
-//             to: to,
-//             visitor: visitor
-//          })
-//       } if (count === 0) {
-//          return res.status(404).send({
-//             message: 'No record found for this request',
-//          })
-//       }
-//    } catch (err) {
-//       console.log(err)
-//       return res.status(500).send({ message: err.message })
-//    }
-// });
-
-// //Get visitor By ID
-// router.get('/:id', authentication, async (req, res) => {
-//    const visitor_id = req.params.id
-//    try {
-//       const visitor = await Visitor.findOne({
-//          where: { visitor_id },
-//          include: [{
-//             model: Device,
-//          }]
-//       });
-//       if(!visitor) {
-//          return res.status(404).send({
-//             statusCode: 404,
-//             message: "Cannot find record, it must have been deleted. ",
-//          })
-//       }
-//       if (visitor) {
-//          return res.status(200).json(visitor)
-//       }
-//    } catch (err) {
-//       console.log(err)
-//       return res.status(500).send({
-//          statusCode: 500,
-//          message: "Cannot find records", err
-//       })
-//    }
-// })
-
-//Delete visitor record
 router.delete('/delete/:id', authentication, async (req, res) => {
    const visitor_id = req.params.id
    const visitor = await Visitor.findOne({ where: { visitor_id } })
@@ -239,7 +48,7 @@ router.delete('/delete/', authentication, async (req, res) => {
       if (visitor) {
          // email = visitor.email
          await Visitor.destroy({ where: { 
-
+            
          } })
          // console.log("Visitor profile deleted successfully")
          return res.status(200).send({
