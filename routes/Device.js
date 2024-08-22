a127859832ab88c1c5d89cc6d5f1d54b66a7bd3a
Stephen Oyeyemi <stephenoyeyemi@gmail.com>
2024-08-22 09:45:34 +0100

    Update Device.js

--- routes/Device.js
+++ routes/Device.js
@@ -1,19 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const deviceController = require('../controllers/deviceController');
 const { authentication } = require('../middleware/authentication');
 const roles_list  = require('../config/roles_list');
 const verifyRoles = require('../middleware/verifyRoles');
 
+router.post('/', authentication, deviceController.createDevice);
 
+router.get('/devices', authentication, deviceController.getDevices);
 
+router.put('/:id', authentication, verifyRoles(roles_list[0]), deviceController.updateDevice);
 
-// Create device
-router.post('/', authentication, );
-
-router.get('/devices', authentication, )
-
-router.put('/:id', authentication, verifyRoles(roles_list[0]) )
-
-router.delete('/:id', authentication, verifyRoles(roles_list[0]))
+router.delete('/:id', authentication, verifyRoles(roles_list[0]), deviceController.deleteDevice)
 
 module.exports = router;
