1075ddd20a348bef4fba46e2dc7dfea76aae8b10
Stephen Oyeyemi <stephenoyeyemi@gmail.com>
2024-08-24 23:11:10 +0100

    Update Visitor.js

--- routes/Visitor.js
+++ routes/Visitor.js
@@ -4,6 +4,12 @@ const visitorController = require ("../controllers/visitorController");
 const { authentication } = require('../middleware/authentication');
 const verifyRoles = require('../middleware/verifyRoles');
 
+router.post("/", authentication, visitorController.createVisitor );
+
+router.get('/visitorhistory', authentication, visitorController.visitorHistory);
+
+router.get('/currentvisitor', authentication, visitorController.currentVisitor);
+
 router.delete('/delete/:id', authentication, visitorController.deleteVistor);
 
 router.delete('/deletemany/', authentication, visitorController.deleteManyVisitor);
