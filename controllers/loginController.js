c9f634e989cd2f7e270442b568a7e61caa0167ab
stevesdiary <stephenoyeyemi@gmail.com>
2024-08-28 00:19:12 +0100

    Update loginController.js
    Code cleanups

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -5,15 +5,13 @@ const bcrypt = require('bcryptjs');
 const loginController = async (req, res) =>{
 	const { email, password} = req.body;
 	const userEmail = await Admin.findOne({where: {email: email}}).catch((err) => {
-		 console.log('Errror: ', err);
+		console.log('Errror: ', err);
 	});
 
-	if(!userEmail)
-		 // console.log('User not found: ', userEmail);
+	if(!userEmail){
 		return res.status(404).send({ Message: 'Email not found!'});
-	
-	const match = await bcrypt.compare(password, userEmail.password);
-	// console.log(userEmail.password, match, password);
+		const match = await bcrypt.compare(password, userEmail.password);
+	}
 	if (!match){
 		return res.status(401).send({ Message: 'Password is incorrect!'});
 	}
@@ -25,21 +23,20 @@ const loginController = async (req, res) =>{
 	const last_name = admin.last_name
 	const role = admin?.role
 	const accessToken = jwt.sign( {
-		 exp: Math.floor(Date.now() / 1000) + (60*60*10),
-		 UserInfo: {
+		exp: Math.floor(Date.now() / 1000) + (60*60*10),
+		UserInfo: {
 				admin_id: admin_id, 
 				email: userEmail.email,
 				role: admin.role
-		 }}, process.env.JWT_SECRET);
-		 // console.log(email + " logged in as " + role)
+		}}, process.env.JWT_SECRET);
 
 	return res.status(200).json({
-		 statusCode: 200,
-		 visitor_id: admin.visitor_id,
-		 first_name: first_name, 
-		 last_name: last_name, 
-		 role: role, 
-		 token: accessToken });
+		statusCode: 200,
+		visitor_id: admin.visitor_id,
+		first_name: first_name, 
+		last_name: last_name, 
+		role: role, 
+		token: accessToken });
 
 };
 
